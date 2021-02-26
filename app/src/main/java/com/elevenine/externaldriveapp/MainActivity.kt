53207e27aee88ac15678b54b2121d946509ae777
Sherzod <sherik0508@gmail.com>
2021-02-26 12:22:07 +0500

    libaums library for working with usb

--- app/src/main/java/com/elevenine/externaldriveapp/MainActivity.kt
+++ app/src/main/java/com/elevenine/externaldriveapp/MainActivity.kt
@@ -12,17 +12,21 @@ import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.github.mjdev.libaums.UsbMassStorageDevice
 import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
 
     private var isUsbDriveConnected = false
-    private var connectedDevice: UsbDevice? = null
+    private var connectedDevice: UsbMassStorageDevice? = null
 
     private var manager: UsbManager? = null
+    private var testInfoString = ""
 
     private val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
 
@@ -31,14 +35,29 @@ class MainActivity : AppCompatActivity() {
         override fun onReceive(context: Context, intent: Intent) {
             if (ACTION_USB_PERMISSION == intent.action) {
                 synchronized(this) {
-                    val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
 
                     if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                        device?.apply {
-                            //call method to set up device communication
+                        // before interacting with a device you need to call init()!
+                        connectedDevice?.init()
+
+                        // Only uses the first partition on the device
+                        val currentFs = connectedDevice?.partitions?.get(0)?.fileSystem
+                        testInfoString += currentFs?.rootDirectory?.absolutePath + "\n"
+                        testInfoString += "Capacity: " + currentFs?.capacity + "\n"
+
+                        Log.d(TAG, "Occupied Space: " + currentFs?.occupiedSpace)
+                        Log.d(TAG, "Free Space: " + currentFs?.freeSpace)
+                        Log.d(TAG, "Chunk size: " + currentFs?.chunkSize)
+
+                        val root = currentFs?.rootDirectory
+                        val usbDirectory = root?.search(findViewById<EditText>(R.id.et_search).text.toString())
+                        if (usbDirectory == null) {
+                            Toast.makeText(this@MainActivity, "Directory was not found!", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(this@MainActivity, usbDirectory.absolutePath + " - " + usbDirectory.name, Toast.LENGTH_SHORT).show()
                         }
                     } else {
-                        Log.d("MainActivity", "permission denied for device $device")
+                        Log.d("MainActivity", "permission denied for device $connectedDevice")
                     }
                 }
             }
@@ -56,11 +75,37 @@ class MainActivity : AppCompatActivity() {
 
 
         findViewById<Button>(R.id.btn_usb).setOnClickListener {
-            checkIfUsbStorageIsConnected()
+            /*checkIfUsbStorageIsConnected()
             if (isUsbDriveConnected)
                 Toast.makeText(this, "UsbDrive is connected", Toast.LENGTH_SHORT).show()
 
-            isFileExists()
+            isFileExists()*/
+
+            if (packageManager.hasSystemFeature(PackageManager.FEATURE_USB_HOST)) {
+                val devices =
+                    UsbMassStorageDevice.getMassStorageDevices(this /* Context or Activity */)
+
+                // just print data for testing
+                devices.forEach { entry ->
+                    Log.d("MainActivity", entry.usbDevice.deviceName)
+                    testInfoString += entry.usbDevice.deviceName + " - " + entry.usbDevice.deviceClass + "\n"
+
+                }
+                testInfoString += "device info: " + "\n"
+
+                for (device in devices) {
+
+                    connectedDevice = device
+
+                    manager = getSystemService(USB_SERVICE) as UsbManager
+                    registerReceiver(usbReceiver, IntentFilter(ACTION_USB_PERMISSION))
+                    val permissionIntent =
+                        PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0)
+                    manager?.requestPermission(device.usbDevice, permissionIntent)
+                }
+
+                findViewById<TextView>(R.id.tv_usb).text = testInfoString
+            }
         }
     }
 
@@ -83,7 +128,13 @@ class MainActivity : AppCompatActivity() {
                 if (device.deviceClass == UsbConstants.USB_CLASS_MASS_STORAGE)
                     isUsbDriveConnected = true
 
-                manager?.requestPermission(device, PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0))
+                manager?.requestPermission(
+                    device, PendingIntent.getBroadcast(
+                        this, 0, Intent(
+                            ACTION_USB_PERMISSION
+                        ), 0
+                    )
+                )
             }
 
             // just print data for testing
@@ -95,5 +146,26 @@ class MainActivity : AppCompatActivity() {
             }
             findViewById<TextView>(R.id.tv_usb).text = string
         }
+    }
+
+
+/*    private fun isAccessGranted(): Boolean {
+        return try {
+            val packageManager = packageManager
+            val applicationInfo = packageManager.getApplicationInfo(packageName, 0)
+            val appOpsManager = getSystemService(APP_OPS_SERVICE) as AppOpsManager
+            var mode = appOpsManager.checkOpNoThrow(
+                AppOpsManager.OPSTR_GET_USAGE_STATS,
+                applicationInfo.uid, applicationInfo.packageName
+            )
+            mode == AppOpsManager.MODE_ALLOWED
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+    }*/
+
+
+    companion object {
+        const val TAG = "MainActivity"
     }
 }
